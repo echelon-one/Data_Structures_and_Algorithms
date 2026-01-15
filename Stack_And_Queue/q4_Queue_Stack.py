c8735bc5539c32c2100c4e2555cd023a32c49936
echelon-one <71792933+echelon-one@users.noreply.github.com>
2026-01-15 21:21:08 +0000

    Create q4_Queue_Stack.py

---
+++ Stack_And_Queue/q4_Queue_Stack.py
@@ -0,0 +1,46 @@
+# INTERVIEW QUESTION 4: QUEUE VIA STACKS
+
+# Implement Queue class which implements a queue using two stacks.
+
+class Stack():
+    def __init__(self):
+        self.list = []
+
+    def __len__(self):
+        return len(self.list)
+
+    def push(self, item):
+        self.list.append(item)
+
+    def pop(self):
+        if len(self.list) == 0:
+            return None
+        return self.list.pop()
+
+
+class QueueViaStack():
+    def __init__(self):
+        self.inStack = Stack()
+        self.outStack = Stack()
+
+    def enqueue(self, item):
+        self.inStack.push(item)
+
+    def dequeue(self):
+        # Reverse order of elements in inStack by pushing them to the outStack
+        while len(self.inStack):
+            self.outStack.push(self.inStack.pop())
+        # dequeue the relevant/top item 
+        result = self.outStack.pop()
+        # Reverse the order of the elements back to the original order
+        while len(self.outStack):
+            self.inStack.push(self.outStack.pop())
+        return result
+    
+
+customQueue = QueueViaStack()
+customQueue.enqueue(1)
+customQueue.enqueue(2)
+customQueue.enqueue(3)
+customQueue.enqueue(4)
+print(customQueue.dequeue())
