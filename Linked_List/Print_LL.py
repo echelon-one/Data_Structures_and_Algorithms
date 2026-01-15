55ed047d67e3651598d4b7c2feaa6a67c2c396b2
echelon-one <71792933+echelon-one@users.noreply.github.com>
2026-01-15 20:46:29 +0000

    Create Print_LL.py

---
+++ Linked_List/Print_LL.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        def __str__(self):
+            return f"Person {self.name} - {self.age}" # NOT WORKING!!!!!!!!!!!
+
+new_person = Person("David", 45)
+print(new_person)
